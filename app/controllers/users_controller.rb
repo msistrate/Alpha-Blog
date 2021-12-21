14d7a82461b7c62aa2d66f05ee9e6557863b01f8
Istrate Mihai <mihai.istrate@eventya.net>
2021-12-21 22:56:58 +0200

    Added Pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,30 @@ class UsersController < ApplicationController
 	def new 
 		@user = User.new
 	end
+
+	def edit
+		@user = User.find(params[:id]) 
+	end
+
+	def show
+		@user = User.find(params[:id])
+		@articles = @user.articles.paginate(page: params[:page], per_page: 5)
+	end
+
+	def index
+		@users = User.paginate(page: params[:page], per_page: 5)
+	end
+	
+	def update
+		@user = User.find(params[:id])
+		
+		if @user.update(user_params)
+			flash[:notice] = "User #{@user.username} successfully edited!"
+			redirect_to @user 
+		else
+			render 'new'
+		end	
+	end	
 	
 	def create
 		@user = User.new(user_params)
